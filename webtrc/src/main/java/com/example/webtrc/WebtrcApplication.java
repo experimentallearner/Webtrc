f016f1a69a522b02858eea887b2bbd115d22faa6
Deep learner <learner.researcher@proton.me>
2025-10-06 00:30:24 +0530

    test

---
+++ webtrc/src/main/java/com/example/webtrc/WebtrcApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrcApplication.class, args);
+	}
+
+}
---
+++ webtrc/src/test/java/com/example/webtrc/WebtrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
